3327b0566638e4d3ae2b34d7df556864989422ce
Tomas Masnik <tomas.masnik@gmail.com>
2015-10-17 18:19:19 +0200

    mongoDb connection first attempt

--- assets/js/welcome/welcome.js
+++ assets/js/welcome/welcome.js
@@ -1,4 +1,16 @@
-angular.module("DCH")
-  .controller("WelcomeCtrl", ['$scope', function ($scope) {
-    $scope.name = "World";
+angular.module("DCH").controller("WelcomeCtrl", [
+  '$scope',
+  '$http',
+  function ($scope, $http) {
+
+    $http.get('/api/dog').then(function (res) {
+      $scope.dogs = res.data;
+    });
+
+    $scope.save = function() {
+      $http.post('/api/dog', {name: $scope.dogName}).then(function (res) {
+        $scope.dogs.push(res.data);
+        $scope.dogName = '';
+      });
+    }
   }]);
--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,13 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  mongoDb: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    host: process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost',
+    port: process.env.OPENSHIFT_MONGODB_DB_PORT || 27017,
+    user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+    password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
+    database: process.env.OPENSHIFT_APP_NAME || 'chovka'
   },
 
   /***************************************************************************
